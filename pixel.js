d7fff4b4f7a176993bcaecfdb14a08e5c1890c7e
yusifmusic <67486650+yusifmusic@users.noreply.github.com>
2020-06-27 09:47:34 +0300

    Update pixel.js

--- pixel.js
+++ pixel.js
@@ -1,26 +1,18 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1404\cocoasubrtf110
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\sl460\partightenfactor0
+  <script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=false;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '273902270341926');
+fbq('track', 'PageView');
+</script>
 
-\f0\fs40 \cf0 \expnd0\expndtw0\kerning0
-  <script>\
-!function(f,b,e,v,n,t,s)\
-\{if(f.fbq)return;n=f.fbq=function()\{n.callMethod?\
-n.callMethod.apply(n,arguments):n.queue.push(arguments)\};\
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';\
-n.queue=[];t=b.createElement(e);t.async=false;\
-t.src=v;s=b.getElementsByTagName(e)[0];\
-s.parentNode.insertBefore(t,s)\}(window, document,'script',\
-'https://connect.facebook.net/en_US/fbevents.js');\
-fbq('init', '273902270341926');\
-fbq('track', 'PageView');\
-</script>\
-\uc0\u8232 \
-<noscript><img height="1" width="1" style="display:none"\
-src="https://www.facebook.com/tr?id=273902270341926&ev=PageView&noscript=1"\
-/></noscript>\
-\uc0\u8232 \
-  <!-- End Facebook Pixel Code -->}
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=273902270341926&ev=PageView&noscript=1"
+/></noscript>
+
+  <!-- End Facebook Pixel Code -->
